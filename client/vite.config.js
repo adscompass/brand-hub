14c63c5c45dec87b6fd03fcae18eaee0ce979177
Daniil Sudorgin <ddsudorgin@gmail.com>
2025-06-25 18:07:55 +0300

    feat: add docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	server: {
+		host: true,
+		port: 5173,
+		watch: {
+			usePolling: true,
+		},
+	},
 });
